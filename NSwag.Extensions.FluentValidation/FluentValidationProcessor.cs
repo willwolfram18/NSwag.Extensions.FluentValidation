93c1ad824ebced1578e5255cf6ba9d5437db491b
willwolfram18 <willwolfram18@gmail.com>
2019-10-15 21:07:47 -0700

    Basic document processor working

---
+++ NSwag.Extensions.FluentValidation/FluentValidationProcessor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Internal;
+using FluentValidation.Validators;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NJsonSchema;
+using NSwag.Generation.AspNetCore;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+
+namespace NSwag.Extensions.FluentValidation
+{
+    public class FluentValidationProcessor : IOperationProcessor
+    {
+        private readonly IValidatorFactory _validatorFactory;
+        private readonly ILogger _log;
+
+        public FluentValidationProcessor(IValidatorFactory validatorFactory,
+            ILoggerFactory loggerFactory = null)
+        {
+            _validatorFactory = validatorFactory ?? throw new ArgumentNullException(nameof(validatorFactory));
+            _log = loggerFactory?.CreateLogger(typeof(FluentValidationProcessor)) ??
+                   NullLogger.Instance;
+        }
+
+        public bool Process(OperationProcessorContext context)
+        {
+            if (context is AspNetCoreOperationProcessorContext aspNetProcessorContext)
+            {
+                return Process(aspNetProcessorContext);
+            }
+
+            return true;
+        }
+
+        private bool Process(AspNetCoreOperationProcessorContext context)
+        {
+            foreach (var operationParameter in context.OperationDescription.Operation.Parameters)
+            {
+                var methodParameter = context.MethodInfo.GetParameters().Single(x => x.Name == operationParameter.Name);
+                var validator = _validatorFactory.GetValidator(methodParameter.ParameterType);
+
+                if (validator == null)
+                {
+                    _log.LogWarning($"Unable to create validator for parameter '{operationParameter.Name}' " +
+                                    $"(type '{methodParameter.ParameterType}') for operation '{context.OperationDescription.Path}'");
+                    continue;
+                }
+
+                foreach (var parameterProperty in operationParameter.ActualSchema.Properties)
+                {
+                    var validationRules = GetValidatorsForProperty(validator, parameterProperty.Key);
+
+                    foreach (var validationRule in validationRules)
+                    {
+                        if (validationRule is INotNullValidator || validationRule is INotEmptyValidator)
+                        {
+                            parameterProperty.Value.IsNullableRaw = false;
+                            parameterProperty.Value.IsRequired = true;
+                        }
+
+                        if (validationRule is INotEmptyValidator)
+                        {
+                            parameterProperty.Value.MinLength = 1;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private IEnumerable<IPropertyValidator> GetValidatorsForProperty(IValidator validator, string propertyName)
+        {
+            var validationRules = validator as IEnumerable<IValidationRule>;
+
+            if (validationRules == null)
+            {
+                return Enumerable.Empty<IPropertyValidator>();
+            }
+
+            return validationRules.OfType<PropertyRule>()
+                .Where(propertyRule =>
+                    !string.IsNullOrWhiteSpace(propertyRule.PropertyName) &&
+                    propertyRule.PropertyName.Equals(propertyName, StringComparison.InvariantCultureIgnoreCase)
+                ).SelectMany(propertyRule => propertyRule.Validators);
+        }
+    }
+}
---
+++ WebApplication1/Validation/ValuesModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using WebApplication1.Models;
+
+namespace WebApplication1.Validation
+{
+    public class ValuesModelValidator : AbstractValidator<ValuesModel>
+    {
+        public ValuesModelValidator()
+        {
+            RuleFor(x => x.Values).NotNull();
+            RuleFor(x => x.Values).NotEmpty();
+        }
+    }
+}
